6f838b3fc555c07cc144a3d8ba902140b2ecb6ca
youngmin-korbit <youngmin.kim@korbit.co.kr>
2022-09-06 14:48:29 +0900

    데이터스토어 및 플로우 테스트

--- app/src/main/java/io/ymsoft/androidstudy/main/MainActivity.kt
+++ app/src/main/java/io/ymsoft/androidstudy/main/MainActivity.kt
@@ -1,15 +1,55 @@
 package io.ymsoft.androidstudy.main
 
+import android.content.Context
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
 import io.ymsoft.androidstudy.base.BaseActivity
 import io.ymsoft.androidstudy.R
 import io.ymsoft.androidstudy.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+        name = "test",
+        produceMigrations = { context ->
+            listOf(SharedPreferencesMigration(context, context.packageName))
+        })
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val test1 = flow<Int> {
+            delay(5000)
+            repeat(100) {
+                emit(it)
+            }
+        }
+        lifecycleScope.launch {
+            val f =  test1.first()
+            println("[MainActivity][onCreate]: $f")
+        }
+
+        val first = runBlocking {
+            dataStore.data.map { it.get(stringPreferencesKey("key")).let { data ->
+                if (!data.isNullOrEmpty())
+                    data.length
+                else
+                    null
+            } }.firstOrNull()
+        }
+
+        println("[MainActivity][onCreate]: $first")
 
     }
 }
