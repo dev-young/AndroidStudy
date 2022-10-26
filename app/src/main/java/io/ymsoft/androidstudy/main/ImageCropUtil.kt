230b2a6cb29f01dbda73f412254d461dff0e2667
youngmin-korbit <youngmin.kim@korbit.co.kr>
2022-10-26 18:21:40 +0900

    temp

---
+++ app/src/main/java/io/ymsoft/androidstudy/main/ImageCropUtil.kt
@@ -0,0 +1,58 @@
+package io.ymsoft.androidstudy.main
+
+import android.content.ContentResolver
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.graphics.Rect
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+
+object ImageCropUtil {
+
+    suspend fun centerCrop(resolver: ContentResolver, uri: Uri, widthPercent: Float, heightPercent: Float) = withContext(Dispatchers.IO) {
+        val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            ImageDecoder.decodeBitmap(ImageDecoder.createSource(resolver, uri))
+        } else {
+            MediaStore.Images.Media.getBitmap(resolver, uri)
+        }
+        centerCrop(bitmap, widthPercent, heightPercent)
+    }
+
+    suspend fun centerCrop(file: File, widthPercent: Float, heightPercent: Float) = withContext(Dispatchers.IO) {
+        centerCrop(BitmapFactory.decodeFile(file.absolutePath), widthPercent, heightPercent)
+    }
+
+    suspend fun centerCrop(bitmap: Bitmap, widthPercent: Float, heightPercent: Float): Bitmap? {
+        val width = bitmap.width * widthPercent
+        val height = bitmap.height * heightPercent
+        return centerCrop(bitmap, width.toInt(), height.toInt())
+    }
+
+    suspend fun centerCrop(bitmap: Bitmap, width: Int, height: Int): Bitmap? {
+        val left = (bitmap.width - width) / 2
+        val top = (bitmap.height - height) / 2
+        return crop(bitmap, left, top, width, height)
+    }
+
+    suspend fun crop(bitmap: Bitmap, rect: Rect): Bitmap? {
+        return crop(bitmap, rect.left, rect.top, rect.width(), rect.height())
+    }
+
+    suspend fun crop(bitmap: Bitmap, _left: Int, _top: Int, _width: Int, _height: Int) = withContext(Dispatchers.Default) {
+        val left = if (_left < 0) 0 else _left
+        val top = if (_top < 0) 0 else _top
+        val width = if (bitmap.width < _width) bitmap.width else _width
+        val height = if (bitmap.height < _height) bitmap.height else _height
+        kotlin.runCatching {
+            Bitmap.createBitmap(bitmap, left, top, width, height)
+        }.onFailure {
+            it.printStackTrace()
+        }.getOrNull()
+    }
+}
+
--- app/src/main/java/io/ymsoft/androidstudy/main/MainActivity.kt
+++ app/src/main/java/io/ymsoft/androidstudy/main/MainActivity.kt
@@ -1,15 +1,143 @@
 package io.ymsoft.androidstudy.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import io.ymsoft.androidstudy.base.BaseActivity
 import io.ymsoft.androidstudy.R
 import io.ymsoft.androidstudy.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.util.concurrent.Executors
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private var imageCapture: ImageCapture? = null
+
+    private val cameraExecutor by lazy { Executors.newSingleThreadExecutor() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSION, 10)
+        }
+
+        binding.ivPreview.setOnClickListener {
+            takePhoto()
+        }
+
     }
+
+    private val tempFile by lazy { File(cacheDir, "temp.jpg") }
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        if (!tempFile.exists()) tempFile.createNewFile()
+        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(tempFile).build()
+
+        imageCapture.takePicture(
+            outputFileOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    outputFileResults.savedUri?.let {
+                        setCropImage(it)
+                    }
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    println("logp-[MainActivity][onError]: ${exception.message}")
+                    exception.printStackTrace()
+                }
+
+            })
+    }
+
+    private fun setCropImage(uri: Uri) {
+        lifecycleScope.launch {
+            val bitmap = ImageCropUtil.centerCrop(
+                tempFile,
+                binding.layoutBox.width.toFloat() / binding.pv.width,
+                binding.layoutBox.height.toFloat() / binding.pv.height
+            )
+            bitmap?.saveTo(tempFile.absolutePath)
+
+            binding.ivPreview.setImageBitmap(bitmap)
+
+        }
+
+    }
+
+    private fun startCamera() {
+        val future = ProcessCameraProvider.getInstance(this)
+        future.addListener({
+            bindPreview(future.get())
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindPreview(provider: ProcessCameraProvider) {
+        var preview = Preview.Builder().build()
+        var cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        imageCapture = ImageCapture.Builder().build()
+
+        preview.setSurfaceProvider(binding.pv.surfaceProvider)
+
+        try {
+            provider.unbindAll()
+            provider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+        } catch (e: Exception) {
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+
+    fun allPermissionsGranted() = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSION = mutableListOf(
+            Manifest.permission.CAMERA
+        ).apply {
+            if (Build.VERSION.SDK_INT < -Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+    }
+}
+
+suspend fun Bitmap.saveTo(path: String, quality: Int = 100) = withContext(Dispatchers.IO) {
+    val file = File(path)
+    kotlin.runCatching {
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        val fos = FileOutputStream(path, false)
+        val bos = BufferedOutputStream(fos)
+        compress(Bitmap.CompressFormat.JPEG, quality, bos)
+        bos.flush()
+        bos.close()
+        true
+    }.getOrElse { false }
 }
